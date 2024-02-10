2f451709bf42ca65bb192d992a803cd2f01ec196
Dilnawaz Khan <dilkhan0706@gmail.com>
2024-02-10 19:54:25 +0500

    code refactoring for context api

---
+++ src/context/ThemeContextProvider.tsx
@@ -0,0 +1,14 @@
+import {useState} from 'react';
+import ThemeContext from './ThemeContext';
+
+const ThemeContextProvider = ({children}: any) => {
+  const [darkMode, setDarkMode] = useState(false);
+
+  return (
+    <ThemeContext.Provider value={{darkMode, setDarkMode}}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export {ThemeContextProvider};
